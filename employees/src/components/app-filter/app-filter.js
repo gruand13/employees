7c72c4a264b9cea21a1e547eedeef9defcc4aa0b
Andrei Grusheuski <gruand13@gmail.com>
2022-10-31 22:37:09 +0300

    start create components( first three are ready

---
+++ employees/src/components/app-filter/app-filter.js
@@ -0,0 +1,23 @@
+import './app-filter.css';
+
+const AppFilter = () =>{
+    return (
+        <div className="btn-group">
+            <button className="btn btn-light"
+            type="button">
+                All employees
+            </button>
+            <button className="btn btn-outline-light"
+            type="button">
+                To promotion
+            </button>
+            <button className="btn btn-outline-light"
+            type="button">
+                Earn more than 1000$
+            </button>
+        </div>
+            
+    )
+}
+
+export default AppFilter;
---
+++ employees/src/components/app-info/app-info.js
@@ -0,0 +1,13 @@
+import './app-info.css';
+
+const AppInfo = ()=>{
+    return (
+        <div className="app-info">
+            <h1>Employees table</h1>
+            <h2>Amount of employees: </h2>
+            <h2>Bonus recieve: </h2>
+        </div>
+    )
+}
+
+export default AppInfo;
